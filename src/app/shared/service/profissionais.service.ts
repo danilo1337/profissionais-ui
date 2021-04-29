1323d67e2db183177c128302252fadfe1e081a67
Danilo Ribeiro <danilorrg@gmail.com>
2021-04-28 21:36:38 -0300

    Adicionado a constante URL_API e alteração ao nome dos recursos.

--- src/app/shared/service/profissionais.service.ts
+++ src/app/shared/service/profissionais.service.ts
@@ -2,15 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaderResponse, HttpHeaders } from '@angular/common/http';
 import { ResponsePageable } from '../model/responsePageable.model';
 import { Observable } from 'rxjs';
-import { Profissao } from '../model/profissao.model';
-import { Profissional } from '../model/profissional.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfissionaisService {
 
-  apiUrl = 'http://localhost:9090';
+  apiUrl = environment.URL_API;
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json'
@@ -21,27 +20,27 @@ export class ProfissionaisService {
   ) {}
 
   public getProfissaoPageable(page:string, size:string): Observable<ResponsePageable>{
-    return this.httpClient.get<ResponsePageable>(this.apiUrl+ '/profissao/?page='+page+'&size='+size, this.httpOptions)
+    return this.httpClient.get<ResponsePageable>(this.apiUrl+ '/profissoes/?page='+page+'&size='+size, this.httpOptions)
   }
   
   public postProfissao(profissao : any){
-    return this.httpClient.post(this.apiUrl+ '/profissao', profissao,this.httpOptions);
+    return this.httpClient.post(this.apiUrl+ '/profissoes', profissao,this.httpOptions);
  }
 
  public putProfissao(profissao : any){
-  return this.httpClient.put(this.apiUrl+ '/profissao', profissao,this.httpOptions);
+  return this.httpClient.put(this.apiUrl+ '/profissoes', profissao,this.httpOptions);
 }
 
   public postProfissional(profissional : any){
-     return this.httpClient.post(this.apiUrl+ '/profissional', profissional,this.httpOptions);
+     return this.httpClient.post(this.apiUrl+ '/profissionais', profissional,this.httpOptions);
   }
 
   public getProfissional(): Observable<ResponsePageable>{
-    return this.httpClient.get<ResponsePageable>(this.apiUrl+ '/profissional', this.httpOptions)
+    return this.httpClient.get<ResponsePageable>(this.apiUrl+ '/profissionais', this.httpOptions)
  }
 
  public getProfissionalPageable(page:string, size:string): Observable<ResponsePageable>{
-  return this.httpClient.get<ResponsePageable>(this.apiUrl+ '/profissional/?page='+page+'&size='+size, this.httpOptions)
+  return this.httpClient.get<ResponsePageable>(this.apiUrl+ '/profissionais/?page='+page+'&size='+size, this.httpOptions)
 }
 
 }
